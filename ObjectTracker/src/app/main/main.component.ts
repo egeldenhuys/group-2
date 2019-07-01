7413d660bb92e7b4feba7d2e93d5982eb88e069e
Emma <u17074526@tuks.co.za>
2019-07-01 15:50:50 +0200

    Style Updates

--- ObjectTracker/src/app/main/main.component.ts
+++ ObjectTracker/src/app/main/main.component.ts
@@ -9,18 +9,20 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 export class MainComponent implements OnInit {
 
-  @ViewChild("sensor1") sensor1: ElementRef;
-
-  sensor1_value: string;
-  sensor2: string;
-  sensor3: string;
-  sensor4: string;
-  sensor5: string;
+  @ViewChild("sensor1", {static: false}) sensor1: ElementRef;
+  @ViewChild("sensor2", {static: false}) sensor2: ElementRef;
+  @ViewChild("sensor3", {static: false}) sensor3: ElementRef;
+  @ViewChild("sensor4", {static: false}) sensor4: ElementRef;
+  @ViewChild("sensor5", {static: false}) sensor5: ElementRef;
 
   constructor() { }
 
   getFormInfo(){
-
+    const sensor1_value = this.sensor1.nativeElement.value;
+    const sensor2_value = this.sensor1.nativeElement.value;
+    const sensor3_value = this.sensor1.nativeElement.value;
+    const sensor4_value = this.sensor1.nativeElement.value;
+    const sensor5_value = this.sensor1.nativeElement.value;
   }
 
   ngOnInit() {
