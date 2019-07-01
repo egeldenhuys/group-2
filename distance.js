8bd8cf38572bcb102113a334848f50592fe30701
Ntokozo513 <50718446+Ntokozo513@users.noreply.github.com>
2019-07-01 14:34:47 +0200

    Add files via upload

--- distance.js
+++ distance.js
@@ -1,14 +1,23 @@
-function diff (num1, num2) {
-  if (num1 > num2) {
-    return (num1 - num2);
+function difference (n1, n2) {
+  if (n1 > n2) {
+    return (n1 - n2);
   } else {
-    return (num2 - num1);
+    return (n2 - n1);
   }
 };
 
-function dist (x1, y1, x2, y2) {
-  var deltaX = diff(x1, x2);
-  var deltaY = diff(y1, y2);
+function distance (x1, y1, x2, y2) {
+  var deltaX = difference(x1, x2);
+  var deltaY = difference(y1, y2);
   var dist = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
   return (dist);
 };
+
+function angle(px, py, p2x, p2y) {
+    var dircy = py - p2y;
+    var dircx = px - p2x;
+    var theta = Math.atan2(dircy, dircx); 
+    theta *= 180 / Math.PI; 
+    
+    return theta;
+  }
