597199b800b20927b973b9ce10d4d55d16334bd8
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2019-07-03 10:36:43 +0200

    Simple, failed

---
+++ opencv-js/main.js
@@ -0,0 +1,46 @@
+// Grab elements, create settings, etc.
+var video2 = document.getElementById('videoInput');
+
+// Get access to the camera!
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        //video.src = window.URL.createObjectURL(stream);
+        video2.srcObject = stream;
+        video2.play();
+    });
+}
+
+let video = document.getElementById('videoInput');
+let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+let cap = new cv.VideoCapture(video);
+let streaming = true;
+
+const FPS = 30;
+function processVideo() {
+
+    try {
+        if (!streaming) {
+            // clean and stop.
+            src.delete();
+            dst.delete();
+            return;
+        }
+        let begin = Date.now();
+        // start processing.
+        cap.read(src);
+        cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+        cv.imshow('canvasOutput', dst);
+        // schedule the next one.
+        let delay = 1000/FPS - (Date.now() - begin);
+        setTimeout(processVideo, delay);
+
+    } catch (err) {
+        console.error(err);
+    }
+
+};
+
+// schedule the first one.
+setTimeout(processVideo, 0);
